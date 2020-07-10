8bae66b29760dea9468587a92531ab7a7a2098b5
aishaola <aishaola@seas.upenn.edu>
2020-07-10 16:46:38 -0400

    Profile can now logout

--- app/src/main/java/com/example/parsegram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/parsegram/fragments/ProfileFragment.java
@@ -1,16 +1,43 @@
 package com.example.parsegram.fragments;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.example.parsegram.LoginActivity;
 import com.example.parsegram.Post;
+import com.example.parsegram.PostsAdapter;
+import com.example.parsegram.R;
 import com.parse.FindCallback;
+import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileFragment extends PostsFragment {
+    TextView tvUsername;
+    Button btnLogout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
 
     @Override
     public void queryPosts() {
@@ -44,5 +71,35 @@ public class ProfileFragment extends PostsFragment {
             }
         });
         adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvUsername = view.findViewById(R.id.tvUser);
+
+        tvUsername.setText(ParseUser.getCurrentUser().getUsername());
+
+        btnLogout = view.findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOutInBackground(new LogOutCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if(e!=null){
+                            Log.e(TAG, "Couldn't logout", e);
+                            return;
+                        }
+
+                        Intent intent = new Intent(getContext(), LoginActivity.class);
+                        Log.i(TAG, "Logged out successfully");
+                        Toast.makeText(getContext(), "Logged out", Toast.LENGTH_SHORT).show();
+                        getContext().startActivity(intent);
+                    }
+                });
+            }
+        });
+
     }
 }
